a5c7c87561e8a9bb149b043984acd12cfbd77ef0
harnoor1533 <4312harnoorlps9c@gmail.com>
2025-02-06 21:37:47 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -7,7 +7,7 @@ int main() {
     scanf("%d %d %c",&num1,&num2,&op);
     switch(op){
         case'+':
-        printf("%d + %d=%d\n",num1,num2,num1+num2);
+        printf("%d + %d=%d\n",num1+num2);
     }
     return 0;
 }
