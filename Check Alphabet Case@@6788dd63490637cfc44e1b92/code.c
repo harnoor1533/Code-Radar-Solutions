c50f69fd300d38f46c51a542f587dcd5e3125296
harnoor1533 <4312harnoorlps9c@gmail.com>
2025-02-10 14:21:39 +0530

    Solution to Check Alphabet Case

--- Check Alphabet Case@@6788dd63490637cfc44e1b92/code.c
+++ Check Alphabet Case@@6788dd63490637cfc44e1b92/code.c
@@ -1,23 +1,19 @@
+#include <stdio.h>
+
 int main() {
     char ch;
     scanf("%c", &ch);
 
     if (ch >= '0' && ch <= '9') {
         printf("Digit\n");
-    }
-
-    else if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
-             ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+    } else if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+               ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
         printf("Vowel\n");
-    }
-
-    else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+    } else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
         printf("Consonant\n");
-    }
-
-    else {
+    } else {
         printf("Special Character\n");
     }
 
-    return 0;
+    return 0;
 }
